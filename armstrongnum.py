cb83d322ff0ee3140153b313955e6fb58202b4c5
KRSHALS8698 <36658781+KRSHALS8698@users.noreply.github.com>
2018-05-17 09:54:08 +0530

    Add files via upload

---
+++ armstrongnum.py
@@ -0,0 +1,21 @@
+l=int(input())
+
+u=int(input())
+
+for num in range(l+1,u):
+  
+  sum=0
+  
+  temp=num
+  
+  while temp>0:
+ 
+     dig=temp%10
+ 
+     sum +=dig**3
+   
+     temp//=10
+  
+  if num==sum:
+  
+     print(num)
