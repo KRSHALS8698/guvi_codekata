a238893ee5ba1ea29b7bcd121cedbb1a4e33c345
KRSHALS8698 <36658781+KRSHALS8698@users.noreply.github.com>
2018-06-23 15:37:00 +0530

    Update and rename week.py to week1.py

--- week.py
+++
@@ -1,9 +0,0 @@
-n=input()
-
-if n in ("Monday","Tuesday","Wednesday","Thursday","Friday"):
-  
-  print("yes")
-
-elif n in("Sunday","Saturday"):
- 
-  print("no")
---
+++ week1.py
@@ -0,0 +1,9 @@
+a=input()
+
+if a in ("Monday","Tuesday","Wednesday","Thursday","Friday"):
+  
+  print("no")
+
+elif n in("Sunday","Saturday"):
+ 
+  print("yes")
