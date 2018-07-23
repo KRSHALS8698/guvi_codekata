7af0053957335251b7636039c0fa57009995583b
KRSHALS8698 <36658781+KRSHALS8698@users.noreply.github.com>
2018-07-23 16:02:16 +0530

    Create repeat.java

---
+++ repeat.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class MyClass {
+    public static void main(String args[]) {
+        int n,i;
+        String a;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        a=sc.next();
+        for(i=0;i<n;i++)
+        {
+            System.out.println(a);
+        }
+    }
+}
