7d0f8b74517926995c7a01f37f4249a1529069b3
KRSHALS8698 <36658781+KRSHALS8698@users.noreply.github.com>
2018-07-23 16:08:39 +0530

    Create concat.java

---
+++ concat.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class MyClass {
+    public static void main(String args[]) {
+        String a,n;
+        Scanner sc=new Scanner(System.in);
+        a=sc.next();
+        n=sc.next();
+        System.out.print(a);
+        System.out.print(n);        
+    }
+}
