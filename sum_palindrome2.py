269dcb641c9b4ebed11914e1be5971429575ee7d
KRSHALS8698 <36658781+KRSHALS8698@users.noreply.github.com>
2019-07-18 10:34:08 +0530

    Update and rename sum_palindrome.py to sum_palindrome2.py

--- sum_palindrome2.py
+++ sum_palindrome2.py
@@ -4,13 +4,13 @@ rev=0
 while(r!=0):
     a=r%10;
     sum=sum+a;
-    r=r/10;
+    r=r//10;
 print sum
 b=sum
 while(b!=0):
     digit=b%10;
     rev=rev*10+digit;
-    b=b/10;
+    b=b//10;
 print rev
 if(rev==sum):
     print ("YES")
